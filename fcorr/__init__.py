b16fbc0ced9bb089406d5d3578c9f277f4bb6118
Dmytro Fishman <dmytro@ut.ee>
2014-08-26 18:05:03 +0300

    Initial package structure

---
+++ fcorr/__init__.py
@@ -0,0 +1,22 @@
+'''
+FCorr: Main module
+
+Copyright 2014, Dmytro Fishman
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'fcorr' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
